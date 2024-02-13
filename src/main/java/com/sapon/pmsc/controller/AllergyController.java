361f93e8e662f180e4391be90880d6e72a9401f4
AlenaSapon <saponalena@gmail.com>
2024-02-13 19:37:03 +0100

    add allergy: completed

--- src/main/java/com/sapon/pmsc/controller/AllergyController.java
+++ src/main/java/com/sapon/pmsc/controller/AllergyController.java
@@ -1,14 +1,16 @@
 package com.sapon.pmsc.controller;
 
 import com.sapon.pmsc.model.Allergy;
-import com.sapon.pmsc.model.Patient;
 import com.sapon.pmsc.repository.AllergyRepository;
 import com.sapon.pmsc.repository.PatientRepository;
 import com.sapon.pmsc.service.AllergyService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/api/v1")
@@ -30,7 +32,9 @@ public class AllergyController {
     public ModelAndView getAllergiesByPatientId(@PathVariable Long id) {
         ModelAndView mav = new ModelAndView("allergies");
         mav.addObject("allergies", allergyRepository.findByPatientId(id));
-        return mav;
+        mav.addObject("patient", patientRepository.findById(id).orElseThrow());
+        mav.addObject("newAllergy", new Allergy());
+                return mav;
     }
 
     @GetMapping("/patients/{id}/allergiesCard")
@@ -39,11 +43,12 @@ public class AllergyController {
         mav.addObject("allergiesCard", allergyRepository.findByPatientId(id));
         return mav;
     }
-    @GetMapping("/allergy/addAllergyModal")
-    public ModelAndView getAllergyModal() {
-        ModelAndView mav = new ModelAndView("fragments/addAllergyModal");
-        mav.addObject("addAllergyModal", new Allergy());
-        mav.addObject("currentPatient");
+
+    @GetMapping("/{id}/allergy/newAllergy")
+    public ModelAndView getAllergyModal(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("fragments/newAllergy");
+       // mav.addObject("newAllergy", new Allergy());
+        mav.addObject("patient", patientRepository.findById(id).orElseThrow());
         return mav;
     }
 
@@ -56,14 +61,15 @@ public class AllergyController {
 
     @PostMapping("/allergy/save")
     public ResponseEntity<Allergy> addNewAllergy(@RequestParam("patientId") Long patientId,
-                                                 @RequestBody Allergy allergyRequest) {
+                                                  Allergy allergyRequest,
+                                                 HttpServletResponse response) throws IOException {
         Allergy allergy = patientRepository.findById(patientId).map(patient -> {
             allergyRequest.setPatient(patient);
             return allergyRepository.save(allergyRequest);
         }).orElseThrow(() -> new IllegalStateException("Not found Patient with id = " + patientId));
+        response.sendRedirect("/api/v1/patients/" + patientId + "/allergies");
         return new ResponseEntity<>(allergy, HttpStatus.CREATED);
     }
-
 
     @GetMapping("/allergies/delete/{allergyId}")
     public ResponseEntity<HttpStatus> deleteAllergy(@PathVariable("allergyId") Long id) {
